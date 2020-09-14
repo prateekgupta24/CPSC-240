837cbbccede2f5f82b1cfffea89bc76ceacae378
Prateek Gupta <47101073+prateekgupta24@users.noreply.github.com>
2020-09-14 16:03:48 -0700

    Update and rename hello.asm to Hello World/hello.asm

--- Hello World/hello.asm
+++ Hello World/hello.asm
@@ -18,3 +18,8 @@ section    .rodata
 
 msg    db 'hello, world', 0xa
 len    equ    $ - msg 
+
+;How to run assembly program in terminals
+;nasm -f elf64 hello.asm # assemble the program  
+;ld -s -o hello hello.o # link the object file nasm produced into an executable file  
+;./hello # hello is an executable file
